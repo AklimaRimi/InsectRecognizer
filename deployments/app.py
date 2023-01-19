2df0e14c44496cc7a5c6162a7b5e2a429948ea50
Rimi <59701116+AklimaRimi@users.noreply.github.com>
2023-01-19 12:30:47 +0600

    Add files via upload

---
+++ deployments/app.py
@@ -0,0 +1,34 @@
+import gradio as gr
+from fastai.vision.all import load_learner
+from fastai import *
+import torch
+import os
+from PIL import Image
+import pathlib 
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+model_path  = 'C:\pythons\Deep Learning\insect\insects-recognizer\models\model3-86_.pkl'
+
+model = load_learner(model_path)
+
+def result(path):  
+  
+  pred,_,probability = model.predict(path)
+
+  return {pred: float(probability.max())}
+
+path = 'test_images/'
+
+image_path = []
+
+for i in os.listdir(path):
+  image_path.append(path+i) 
+
+
+image = gr.inputs.Image(shape =(128,128))
+label = gr.outputs.Label()
+
+iface = gr.Interface(fn=result, inputs=image, outputs=label, examples = image_path)
+iface.launch(inline = False)
